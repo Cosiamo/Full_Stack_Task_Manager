7256862d1fb310fbe8b9f84be461122af6864fac
Cosiamo <61847214+Cosiamo@users.noreply.github.com>
2022-06-20 11:45:51 -0500

    modified data controller

--- lib/controllers/data_controller.dart
+++ lib/controllers/data_controller.dart
@@ -16,6 +16,7 @@ class DataController extends GetxController {
   List<dynamic> get myData => _myData;
   Map<String, dynamic> _singleData = {};
   Map<String, dynamic> get singleData => _singleData;
+
   Future<void> getData() async {
     // _isLoading is set to true because it's loading
     _isLoading = true;
@@ -27,12 +28,13 @@ class DataController extends GetxController {
       print(response.statusCode);
       print("We got the data");
       // will update the data
-      update();
+      // update();
     } else {
       print(response.statusCode);
       print("We didn't get the data");
     }
     _isLoading = false;
+    update();
   }
 
   Future<void> getSingleData(String id) async {
@@ -41,7 +43,8 @@ class DataController extends GetxController {
     // since service.getData is returning a Future, we need to use await
     // string interpolation
     // might need to remove the `?`
-    Response response = await service.getData('${AppConstants.GET_TASK}''?id=$id');
+    Response response =
+        await service.getData('${AppConstants.GET_TASK}' '?id=$id');
     if (response.statusCode == 200) {
       // whatever we get from the server we put inside this List
       //_myData = response.body;
@@ -50,31 +53,82 @@ class DataController extends GetxController {
         _singleData = response.body;
       }
       // will update the data
-      update();
+      // update();
     } else {
       print(response.statusCode);
       print("We didn't get any data");
     }
     _isLoading = false;
+    update();
   }
 
   Future<void> postData(String task, String taskDetail) async {
     // _isLoading is set to true because it's loading
     _isLoading = true;
     // since service.getData is returning a Future, we need to use await
-    Response response = await service.postData(AppConstants.POST_TASK,{
-      "task_name":task,
+    Response response = await service.postData(AppConstants.POST_TASK, {
+      "task_name": task,
       "task_detail": taskDetail,
     });
     if (response.statusCode == 200) {
       print(response.statusCode);
       print("Data post successful");
       // will update the data
-      update();
+      // update();
     } else {
       print(response.statusCode);
       print("Data post failed");
     }
+    update();
+    // might need to add: _isLoading = false;
+  }
+
+  Future<void> updateData(String task, String taskDetail, int id) async {
+    // _isLoading is set to true because it's loading
+    _isLoading = true;
+    // since service.getData is returning a Future, we need to use await
+    Response response =
+        await service.updateData('${AppConstants.UPDATE_TASK}' '?id=$id', {
+      "task_name": task,
+      "task_detail": taskDetail,
+    });
+    if (response.statusCode == 200) {
+      print(response.statusCode);
+      print("Data post successful");
+      // will update the data
+      // update();
+    } else {
+      print(response.statusCode);
+      print("Data post failed");
+    }
+    update();
+    // might need to add: _isLoading = false;
+  }
+
+  Future<void> deleteData(int id) async {
+    // _isLoading is set to true because it's loading
+    _isLoading = true;
+    update();
+    // since service.getData is returning a Future, we need to use await
+    Response response =
+        await service.deleteData('${AppConstants.DELETE_TASK}' '?id=$id');
+    if (response.statusCode == 200) {
+      print(response.statusCode);
+      // will update the data
+      // update();
+    } else {
+      print(response.statusCode);
+      print("Data deletion failed");
+    }
+    // using async/await to make sure we get everything right after a response from the server
+    await Future.delayed(Duration(seconds: 1),
+      () {
+        // setting to false because it's the default state
+        _isLoading = false;
+        // calling update twice so it knows the variable has changed
+        update();
+      }
+    );
     // might need to add: _isLoading = false;
   }
 }
