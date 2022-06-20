83bd07516b1a6be23e1cb92420b880a53659e90c
Cosiamo <61847214+Cosiamo@users.noreply.github.com>
2022-06-20 11:47:06 -0500

    modified app constants

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -1,8 +1,12 @@
 class AppConstants {
   // might need to add a forward slash at the end of the base url
-  // for android: 10.0.2.2
+  // for emulated android: 10.0.2.2
+  // for emulated iOS and everything else: 127.0.0.1 or localhost
+  // for physical android devices use IPv4 address with port num
   static const String BASE_URL = "http://10.0.2.2:8080";
   static const String GET_TASKS = "/gettasks";
   static const String POST_TASK = "/create";
   static const String GET_TASK = "/gettask/";
+  static const String UPDATE_TASK = "/update/";
+  static const String DELETE_TASK = "/delete/";
 }
